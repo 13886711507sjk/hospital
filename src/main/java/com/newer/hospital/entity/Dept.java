5d74b3465b3a8cefbbe594ee62d79a4568e324c7
13886711507sjk <www.2645968117@qq.com>
2020-11-11 15:22:40 +0800

    上传工程

---
+++ src/main/java/com/newer/hospital/entity/Dept.java
@@ -0,0 +1,11 @@
+package com.newer.hospital.entity;
+
+/**
+ * @author SongJinKang
+ * @version 1.0.0
+ * @ClassName Dept.java
+ * @Description TODO
+ * @createTime 2020年11月11日 14:50:00
+ */
+public class Dept {
+}
